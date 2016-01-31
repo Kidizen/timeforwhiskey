77b483899a38d741b008d315c65d2297e0142a7a
Shane Ewert <shane.d.ewert@gmail.com>
2016-01-31 13:38:32 -0600

    Added ability to move JIRA issues

--- scripts/jira.js
+++ scripts/jira.js
@@ -1,4 +1,56 @@
 module.exports = function(robot) {
+
+    function getJIRAAuth() {
+        var username = process.env['JIRA_USERNAME'];
+        var password = process.env['JIRA_PASSWORD'];
+        return new Buffer(username + ':' + password).toString('base64');
+    }
+
+    function transitionStory(reply) {
+
+        var transition;
+        var key = (reply.match[1] || '').toLowerCase().trim();
+        var rawTransition = (reply.match[2] || '').toLowerCase().trim();
+        var sanitized = rawTransition.split(' ').join('');
+
+        if (sanitized === 'todo') {
+            transition = 11;
+        } else if (sanitized === 'inprogress') {
+            transition = 21;
+        } else if (sanitized === 'readyfortest') {
+            transition = 41;
+        } else if (sanitized === 'done') {
+            transition = 31;
+        }
+
+        if (!transition) {
+            reply.send("Sorry, I don't know about the state '" + rawTransition + "'");
+            return;
+        }
+
+        reply.send('One sec...');
+        robot.http('https://itizen.atlassian.net/rest/api/2/issue/KID-' + key + '/transitions')
+            .header('Authorization', 'Basic ' + getJIRAAuth())
+            .header('Content-Type', 'application/json')
+            .post(JSON.stringify({
+                transition: {id: transition}
+            }))(function(err, res, body) {
+                res = res || {};
+                if (!err && res.statusCode === 204) {
+                    reply.send('Success! https://itizen.atlassian.net/browse/KID-' + key);
+                } else {
+                    reply.send("I failed. I couldn't move that issue.");
+                }
+            });
+    }
+
+    robot.respond(/mark (?:KID\-)?([0-9]+) as (.*)/i, function(reply) {
+        transitionStory(reply);
+    });
+    robot.respond(/move (?:KID\-)?([0-9]+) to (.*)/i, function(reply) {
+        transitionStory(reply);
+    });
+
     robot.respond(/create ([A-Za-z]+) for ([A-Za-z]+) (.*)/i, function(reply) {
 
         var type, component;
@@ -30,21 +82,16 @@ module.exports = function(robot) {
             return;
         }
 
-        var username = process.env['JIRA_USERNAME'];
-        var password = process.env['JIRA_PASSWORD'];
-        var auth = new Buffer(username + ':' + password).toString('base64');
-
         reply.send('One sec...');
-
         robot.http('https://itizen.atlassian.net/rest/api/2/issue/')
-            .header('Authorization', 'Basic ' + auth)
+            .header('Authorization', 'Basic ' + getJIRAAuth())
             .header('Content-Type', 'application/json')
             .post(JSON.stringify({
-                'fields': {
-                    'project': {'key': 'KID'},
-                    'summary': text,
-                    'issuetype': {'name': type},
-                    'components':[{'name': component}]
+                fields: {
+                    project: {key: 'KID'},
+                    summary: text,
+                    issuetype: {name: type},
+                    components:[{name: component}]
                 }
             }))(function(err, res, body) {
                 res = res || {};
