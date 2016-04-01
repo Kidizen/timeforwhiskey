5d1831fcef38596b1b1b298d65ec18b1f9d1ee1e
Shane Ewert <shane.d.ewert@gmail.com>
2016-04-01 14:56:01 -0500

    FML. More typos

--- scripts/app-version.js
+++ scripts/app-version.js
@@ -3,24 +3,24 @@ module.exports = function(robot) {
     var IOS_VERSION = '4.3.16';
     var ANDROID_VERSION = '3.16.723';
 
-    function androidVerion(reply) {
+    function androidVersion(reply) {
         reply.send(ANDROID_VERSION);
     }
 
-    function iOSVerion(reply) {
+    function iOSVersion(reply) {
         reply.send(IOS_VERSION);
     }
 
-    robot.hear(/what is the .* android version.*/i, function(reply) {
+    robot.hear(/what is the.*android version.*/i, function(reply) {
         androidVersion(reply);
     });
-    robot.hear(/what's the .* android version.*/i, function(reply) {
+    robot.hear(/what's the.*android version.*/i, function(reply) {
         androidVersion(reply);
     });
-    robot.hear(/what is the .* ios version.*/i, function(reply) {
+    robot.hear(/what is the.*ios version.*/i, function(reply) {
         iOSVersion(reply);
     });
-    robot.hear(/what's the .* ios version.*/i, function(reply) {
+    robot.hear(/what's the.*ios version.*/i, function(reply) {
         iOSVersion(reply);
     });
 }
